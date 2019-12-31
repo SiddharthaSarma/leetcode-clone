29ad098c304779bf214e1f322ba4687b8ae947ff
siddharthasarma <sidduinevitable@gmail.com>
2019-12-31 10:20:05 +0530

    add filter component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Table from "./Table.svelte";
   import Navbar from "./Navbar.svelte";
+  import Filter from "./Filter.svelte";
 </script>
 
 <style>
@@ -10,6 +11,7 @@
 <main>
   <Navbar />
   <div class="container">
+    <Filter />
     <Table />
   </div>
 </main>
