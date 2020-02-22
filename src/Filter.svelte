1bf1f9b40791c94913641a8ce3f4d2fef1001450
siddharthasarma <sidduinevitable@gmail.com>
2020-02-22 18:07:16 +0530

    add filter

--- src/Filter.svelte
+++ src/Filter.svelte
@@ -1,1 +1,27 @@
-Filter
+<script>
+  let search;
+
+  function handleChange() {
+    console.log(search);
+  }
+</script>
+
+<style>
+  .search-bar-control {
+    margin-bottom: 20px;
+  }
+  .list-search-bar {
+    border-radius: 1020px;
+  }
+</style>
+
+<div class="row search-bar-control">
+  <div class="col-sm-6 col-lg-7">
+    <input
+      type="text"
+      class="form-control list-search-bar"
+      placeholder="Search question titles, description or IDs"
+      bind:value={search}
+      on:change={handleChange} />
+  </div>
+</div>
